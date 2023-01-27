d55dbd5840b3d1a5223a235e6f748755e81141f7
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-27 09:45:44 +0200

    Fix overflowing element

--- js/graphics.js
+++ js/graphics.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import '../css/graphicsStyle.css';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -17,17 +16,8 @@ const renderer = new THREE.WebGLRenderer({
   antialias: false
 });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth - 20 , window.innerHeight );
 renderer.setPixelRatio(window.devicePixelRatio);
-
-// const bigobject = new THREE.SphereGeometry(0.4, 25, 25);
-// const texture = new THREE.TextureLoader().load('/img/JavaScript-code.jpg');
-// const material = new THREE.MeshBasicMaterial({ map: texture });
-// const sphere = new THREE.Mesh(bigobject, material);
-// sphere.position.setX(-3.5);
-// sphere.position.setY(1.4);
-// sphere.position.setZ(-1);
-// scene.add(sphere);
 
 const smallobject = new THREE.SphereBufferGeometry(0.1, 35, 20);
 
@@ -38,22 +28,17 @@ smallsphere.position.setX(2);
 smallsphere.position.setY(1);
 scene.add(smallsphere);
 
-// const texture2 = new THREE.TextureLoader().load('/img/img3.jpg');
-// const material2 = new THREE.MeshBasicMaterial({ map: texture2 });
 const smallsphere2 = new THREE.Mesh(smallobject, material1);
 smallsphere2.position.setX(2.4);
 smallsphere2.position.setY(1);
 scene.add(smallsphere2);
 
-// const texture3 = new THREE.TextureLoader().load('/img/img4.jpg');
-// const material3 = new THREE.MeshBasicMaterial({ map: texture3 });
+
 const smallsphere3 = new THREE.Mesh(smallobject, material1);
 smallsphere3.position.setX(2.8);
 smallsphere3.position.setY(1);
 scene.add(smallsphere3);
 
-// const texture4 = new THREE.TextureLoader().load('/img/img5.jpg');
-// const material4 = new THREE.MeshBasicMaterial({ map: texture4 });
 const smallsphere4 = new THREE.Mesh(smallobject, material1);
 smallsphere4.position.setX(3.2);
 smallsphere4.position.setY(1);
@@ -69,14 +54,6 @@ smallsphere6.position.setX(4);
 smallsphere6.position.setY(1);
 scene.add(smallsphere6);
 
-// const geometry = new THREE.BoxGeometry( 0.02, 1, 1 );
-// const texture5 = new THREE.TextureLoader().load('/img/code1.png');
-// const material0 = new THREE.MeshBasicMaterial({ map: texture5 });
-// const cube = new THREE.Mesh( geometry, material0 );
-// cube.position.setX(-1.5)
-// cube.position.setY(-0.8)
-// cube.position.setZ(-0.8)
-// scene.add( cube );
 
 const translate = () => {
   setTimeout(() => {
